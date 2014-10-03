69894c51e113a35329218050d5d99aefccfc650b
Enes Huseinčehajić <bezbeli@gmail.com>
2014-10-03 19:01:39 +0200

    sastavio 701 i 702

--- single-municipalities.php
+++ single-municipalities.php
@@ -6,20 +6,39 @@
 $custom_fields = get_post_custom();
 // echo '<div class="js-masonry">';
 
+
+if ( in_array(array("701"), $custom_fields)) {
+    	array_push($custom_fields[president_bih], "702");
+} elseif (in_array(array("702"), $custom_fields)) {
+    	array_push($custom_fields[president_bih], "701");
+}
+
+	// echo "<pre style='font-size:10px;'>";
+	// 	print_r($custom_fields);
+	// echo "</pre>";
+
 foreach ( $custom_fields as $field_key => $field_values ) {
+
+
+
 	foreach ( $field_values as $key => $value )
 		if ($value && $field_key != 'municipality_ID' && $field_key != '_edit_lock') {
 		echo '<div class="item utrka col-md-3">';
+					
+					// if ($value == '701' || $value == '702' ) {
+					// 	$value = array('701','702'); 
+					// }
 
 					$args = array(
 						'post_type' => 'izborne_utrke',
 						'meta_query' => array(
 							array(
 								'key'  => 'election_id',
-								'value'  => $value,
+								'value'  => $value
 								),
 							),
 					 );
+
 					$query_election_name = new WP_Query( $args );
 					while ( $query_election_name->have_posts() ) : $query_election_name->the_post();
 					echo '<h5>';
@@ -60,6 +79,7 @@ foreach ( $custom_fields as $field_key => $field_values ) {
 				echo '<ul>';
 				foreach ($partije as $key => $value) {
 					echo '<li class="partija">' ;
+					// echo get_post_meta( $post->ID, 'election_id', true ) . '<br />';
 					echo $key;
 					echo '</li>';
 					$args = array(
@@ -91,7 +111,6 @@ foreach ( $custom_fields as $field_key => $field_values ) {
 
 				if (get_the_content( ) || $parliament_logic_drugi_dom || $government_logic) {
 					echo '<li class="zastupnik">';
-						echo ' &#9617; ';
 						echo get_post_meta( $post->ID, 'list_number', true ) . ' ';
 						echo '<a href="' . get_permalink() . '">';
 						echo get_the_title( );
@@ -104,7 +123,6 @@ foreach ( $custom_fields as $field_key => $field_values ) {
 					echo '</li>';
 				} else {
 					echo '<li class="zastupnik">';
-						echo ' &#9617; ';
 						echo get_post_meta( $post->ID, 'list_number', true ) . ' ';
 						echo get_the_title( );
 						// echo '<br />';
